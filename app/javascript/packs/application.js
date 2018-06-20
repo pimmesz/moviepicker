483fc281d040171f9f66843a159e60d4ed2c64cb
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-20 10:19:23 +0200

    Added pages and movies controllers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,3 @@
-// app/javascript/packs/application.js
 import 'bootstrap';
 
 console.log('Hello World from Webpacker')
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'movies/index'
+  get 'movies/show'
+  get 'pages/contact'
+  get 'pages/about'
+  root :to => "movies#index"
 end
