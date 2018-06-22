307e57049e743dcdb49511b6327e88602f4b7293
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-22 11:45:40 +0200

    Changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
+require 'json'
+require 'open-uri'
+
 class MoviesController < ApplicationController
   def index
     @movies = %w[amsterdam paris berlin newyork]
+
+    url = 'https://api.themoviedb.org/3/search/multi?api_key=2d80876e5f6bec711a87959701829d27&language=en-US&query=matrix&page=1&include_adult=false'
+    movie_serialized = open(url).read
+    movie = JSON.parse(user_serialized)
   end
 
   def show
