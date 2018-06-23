4e6981a8cf511846760a7bae19e0f339b323c1e3
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-24 00:36:21 +0200

    Add movie poster

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,8 +4,9 @@ class SearchesController < ApplicationController
 
     Tmdb::Api.key("2d80876e5f6bec711a87959701829d27")
     @search_results = Tmdb::Search.new
-    # @search_results.resource('person') # determines type of resource
-    @search_results.resource('movie')
+    @search_results.resource('person') # determines type of resource
+    # @search_results.resource('movie')
+    # @search_results.resource('genre')
     @search_results.query(params[:query]) # the query to search against
     @results = @search_results.fetch # makes request
   end
